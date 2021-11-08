e6384c0b7c3db513424f2b049ba2b5be97318905
roman <changjun19920716@gmail.com>
2021-11-08 19:19:37 +0800

    🎨 清理框架内容

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/antd.css'
-// import './components/custom-list'
-// import bus from './utils/bus'
 
 const app = createApp(App)
-
-// require context 注册自定义组件
-const requireContext = require.context(
-  './components/custom-list',
-  true,
-  /index.vue$/
-)
-const customComponentList = requireContext.keys().filter((item: any) => true)
-
-customComponentList.forEach((item: any) => {
-  const componentConfig = requireContext(item)
-  const name = item.split('/')[1]
-  app.component(name, componentConfig.default || componentConfig)
-})
-
-// app.config.globalProperties.$bus = bus
 app.use(store).use(router).mount('#app')
--- src/store/modules/editor.ts
+++ src/store/modules/editor.ts
@@ -1,61 +1,13 @@
 import { ActionContext } from 'vuex'
-import { $ } from '../../utils/utils'
 
-export class CanvasStyle {
-  constructor(width: number, height: number, scale: number) {
-    this.width = width
-    this.height = height
-    this.scale = scale
-  }
-  width: number
-  height: number
-  scale: number
-}
 /**
  * 组件持久化
  */
-interface EditorState {
-  canvasStyleData: CanvasStyle
-  editor: any
-}
-const state: EditorState = {
-  canvasStyleData: new CanvasStyle(1200, 740, 200),
-  editor: null
-}
-const getters = {
-  canvasStyleData: (state: EditorState) => {
-    return state.canvasStyleData
-  },
-  editor: (state: EditorState) => {
-    return state.editor
-  }
-}
-const mutations = {
-  GET_EDITOR(state: EditorState) {
-    state.editor = $('#editor')
-  },
-  SET_COURSE_STYLE(state: EditorState, style: CanvasStyle) {
-    state.canvasStyleData = style
-  }
-}
-const actions = {
-  getEditor({ commit }: ActionContext<EditorState, any>) {
-    commit('GET_EDITOR')
-  },
-  /**
-   *设置画布样式
-   * @param param
-   * @param style
-   */
-  setCanvasStyle(
-    { commit }: ActionContext<EditorState, any>,
-    style: CanvasStyle
-  ) {
-    if (style) {
-      commit('SET_COURSE_STYLE', style)
-    }
-  }
-}
+interface EditorState {}
+const state: EditorState = {}
+const getters = {}
+const mutations = {}
+const actions = {}
 
 export default {
   actions,
